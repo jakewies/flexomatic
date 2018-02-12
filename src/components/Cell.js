b6205cf6381d1a0697bebbc6edbf2029052792b0
Jake Wiesler <jakewiesler@gmail.com>
2018-02-12 06:25:59 -0500

    Update the way that the Cell component handles dynamic styles through the use of props.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,20 +1,57 @@
+import React from 'react'
 import styled from 'styled-components'
-import { number, bool } from 'prop-types'
+import { number, bool, node } from 'prop-types'
 
-const Cell = styled.div`
-  display: ${props => (props.flexed ? 'flex' : 'block')};
-  flex: 1;
-  padding: 1em 0 0 1em;
-  width: ${props => (props.size ? calcSize(props.size) : 'auto')};
-`
+export default function Cell(props) {
+  const dynamicStyles = getDynamicStyles(props)
+  const styles = generateStyles(defaultStyles, dynamicStyles)
+  const Component = styled.div`
+    ${styles};
+  `
+  return <Component>{props.children}</Component>
+}
 
 Cell.propTypes = {
   size: number,
-  flexed: bool
+  flexed: bool,
+  children: node
+}
+
+const defaultStyles = {
+  'box-sizing': 'border-box',
+  display: 'block',
+  flex: 1,
+  padding: '1em 0 0 1em',
+  width: 'auto'
+}
+
+function getDynamicStyles(options) {
+  const { flexed, size } = options
+  const styles = {}
+
+  if (flexed) {
+    styles.display = 'flex'
+  }
+
+  if (size) {
+    styles.width = calcSize(size)
+    styles.flex = 'none'
+  }
+
+  return styles
+}
+
+function generateStyles(defaultStyles, dynamicStyles) {
+  const stylesAsObj = Object.assign({}, defaultStyles, dynamicStyles)
+  return convertStylesToTemplateLiteral(stylesAsObj)
+}
+
+function convertStylesToTemplateLiteral(styles) {
+  return Object.keys(styles)
+    .reduce((accum, key) => `${accum} ${key}: ${styles[key]};`, ``)
+    .trim()
 }
 
 function calcSize(size) {
   return `${size * 100}%`
 }
-
-export default Cell
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
 const Grid = styled.div`
+  box-sizing: border-box;
   display: flex;
   flex-wrap: wrap;
   list-style: none;
   margin: -1em 0 1em -1em;
+  padding: 0;
 
   &:last-child {
     margin-bottom: 0;
